4f96721591adda7c9defda8c5eda44b398eda7fe
Kallirroi <kallirroi.retzepi@gmail.com>
2020-06-02 17:41:23 -0400

    correctly renders local or remote data, have to properly parse very nested objects

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [selectValue, setSelectValue] = useState('');
+
+  const fetchData = async (url) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result)
+      })
+  }
+
+  const handleChange = (e) => {
+    setSelectValue(e.target.value);
+    setData([])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>JSON/Wordpress API playground</header>
+
+      <select name="endpoints" onChange={handleChange}>   
+        <option value="">select a variable</option>
+        <option value="categories">categories</option>
+        <option value="comments">comments</option>
+        <option value="media">media</option>
+        <option value="posts">posts</option>
+        <option value="tags">tags</option>
+      </select>
+
+      <button onClick={() => {setData([]); fetchData(`http://localhost:3002/${selectValue}`)} }>show local JSON</button>
+      <button onClick={() => {setData([]); fetchData(`https://wordsinspace.net/shannon/wp-json/wp/v2/${selectValue}`)}}>call Wordpress API </button>
+      
+      <header>{data.length > 0 ? `${data.length} entries` : null }</header>
+      {data.length > 0 
+        ? 
+        data.map( (item) => (
+          <div className='card' key={item.id}>
+            <div className='card-header'>
+              {Object.keys(item).map((headerKey, index) => {
+                return (<div key={index}>{headerKey}</div>) 
+              })}
+            </div>   
+            <div className='card-results' >
+              {Object.keys(item).map((headerKey) => {
+                const obj = item[headerKey]
+                return (typeof obj === 'string' || typeof obj === 'number'  ? <div>{obj}</div> : <div>OBJECT</div>)
+              })}              
+            </div>
+          </div>
+        ))
+        : null
+      }
     </div>
   );
 }
