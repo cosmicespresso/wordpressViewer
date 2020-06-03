b0340ef1193879a074d3ba852883885ecdb24680
Kallirroi <kallirroi.retzepi@gmail.com>
2020-06-03 13:12:27 -0400

    done with pagination and showing total counts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
@@ -6,10 +6,18 @@ function App() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [selectValue, setSelectValue] = useState('');
-
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const pageLimit = 20;
+  
   const fetchData = async (url) => {
     fetch(url)
-      .then((res) => res.json())
+      .then((res) => {
+        setTotal(res.headers.get('X-WP-Total'))
+        setTotalPages(res.headers.get('X-WP-TotalPages'))
+        return res.json()
+      })
       .then((result) => {
         setData(result);
         setIsLoading(false);
@@ -17,10 +25,27 @@ function App() {
   }
 
   const handleChange = (e) => {
+    e.preventDefault();
+    setPage(1); 
     setSelectValue(e.target.value);
-    setData([])
+    setData([]);
   }
 
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+    setData([]); 
+    setIsLoading(true); 
+    fetchData(`https://wordsinspace.net/shannon/wp-json/wp/v2/${selectValue}?per_page=${pageLimit}&page=${page}&orderby=id&order=desc`);
+  }
+
+  const handlePages = (e) => {
+    e.preventDefault();
+    setPage(page + 1);
+    setData([]); 
+    setIsLoading(true); 
+    fetchData(`https://wordsinspace.net/shannon/wp-json/wp/v2/${selectValue}?per_page=${pageLimit}&page=${page+1}&orderby=id&order=desc`);
+  }
+  
   return (
     <div className="App">
       <header>JSON/Wordpress API playground</header>
@@ -34,14 +59,14 @@ function App() {
         <option value="tags">tags</option>
       </select>
 
-      <button onClick={() => {setData([]); setIsLoading(true); fetchData(`http://localhost:3002/${selectValue}`)} }>show local JSON</button>
-      <button onClick={() => {setData([]); setIsLoading(true); fetchData(`https://wordsinspace.net/shannon/wp-json/wp/v2/${selectValue}?per_page=100`)}}>call Wordpress API </button>
+      {/*<button onClick={() => {setData([]); setIsLoading(true); fetchData(`http://localhost:3002/${selectValue}`)} }>show local JSON</button>*/}
+      <button onClick={handleButtonClick}>call Wordpress API </button>
       
-      <header>{data.length > 0 ? `${data.length} entries` : null }</header>
+      <header>{data.length > 0 ? `There are ${total} entries across ${totalPages} page(s)` : null }</header>
       {!isLoading
         ? 
-        data.map( (item) => (
-          <div className='card' key={item.id}>
+        data.map( (item, i) => (
+          <div className='card' key={i}>
             <div className='card-header'>
               {Object.keys(item).map((headerKey, index) => {
                 return ( headerKey !== 'meta' && headerKey !== '_links' 
@@ -56,10 +81,10 @@ function App() {
                   ? (typeof obj === 'string' || typeof obj === 'number')  
                      ? <div key={index}>{headerKey.includes('date') || headerKey.includes('modified') ? obj.slice(0,10) : obj}</div>
                      : <ul className='object' key={index}> 
-                       {obj && Object.keys(obj).map((objKey, index) => {
+                       {obj && Object.keys(obj).map((objKey, objIndex) => {
                           return (
                             typeof obj[objKey] === 'string' || typeof obj[objKey] === 'number' 
-                            ? <li key={index}>{obj[objKey]}</li>
+                            ? <li key={objIndex}>{obj[objKey]}</li>
                             : 'TOO NESTED!'
                           ) 
                         })
@@ -72,6 +97,12 @@ function App() {
           </div>
         ))
         : <div>Loading....</div>
+      }
+      {!isLoading && data.length > 0 &&  
+        <div>
+          <button className='loadmore' onClick={handlePages}>Load More</button>
+          <div className='pagecount'>page {page}</div>
+        </div>
       }
     </div>
   );
